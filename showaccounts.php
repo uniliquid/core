4afa0482717fc91e24f6850b77a283782a5f0a8c
lava <lava@gruss.cc>
2013-03-19 08:32:40 +0100

    voting right stats updated

--- showaccounts.php
+++ showaccounts.php
@@ -7,6 +7,8 @@ if (strlen($datakey1) == 0 || strlen($datakey2) == 0)
 if (strlen($sData) < 1000)
   die('Daten von Mitgliederverwaltung zu kurz!');
 
+$oo_nums = array("AT130" => 2, "AT221" => 11);
+
 $decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($datakey2), base64_decode($sData), MCRYPT_MODE_CBC, md5(md5($datakey2))), "\0");
 $lines = explode("\n",$decrypted);
 foreach ($lines as $line)
@@ -19,7 +21,11 @@ foreach ($lines as $line)
   $mail = filter_var($mail_original, FILTER_SANITIZE_EMAIL);
   if (!filter_var($mail, FILTER_VALIDATE_EMAIL)/* || $mail == "mitglied@piratenpartei.at"*/) { echo "FATAL: email address invalid: '$mail':\n"; print_r($values); continue; }
   $lo = $values[3];
-  $oo = $values[4];
+  $oo = explode(",",$values[4]);
+  if (count($oo) == 0 || strlen($oo[0]) != 5)
+    $oo[0] = 'ATXXX';
+  if (count($oo) == 1 || strlen($oo[1]) != 5)
+    $oo[1] = 'ATXXX';
   $akk = ($values[5] != null);
   $paid = ($values[6] != null);
   if (!$akk || !$paid)
@@ -37,12 +43,9 @@ foreach ($lines as $line)
     case 37: $lo_num = 2; break;
     default: $lo_num = 0; break;
   }
-  switch ($oo)
-  {
-    case 90: $oo_num = 11; break;
-    default: $oo_num = 0; break;
-  }
-  echo "insert: identification=$idc, email=$mail, lo=$lo_num, oo=$oo_num, akk=$akk, paid=$paid\n";
+  $oo_num1 = isset($oo_nums[$oo[0]]) ? $oo_nums[$oo[0]] : 0;
+  $oo_num2 = isset($oo_nums[$oo[1]]) ? $oo_nums[$oo[1]] : 0;
+  echo "insert: identification=$idc, email=$mail, unit=$lo_num, unit=$oo_num1, unit=$oo_num2, akk=$akk, paid=$paid\n";
 }
 
 ?>
