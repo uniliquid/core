525a5f0e838a465139ea765ef5a2bc248fbccd03
lava <lava@gruss.cc>
2013-12-18 23:41:06 +0100

    new mail notifications

--- sendinvitation.sh
+++ sendinvitation.sh
@@ -4,6 +4,6 @@ if [ $# -eq 1 ]; then
   JETZT=`date`
   echo "$JETZT send key to user with lqfb-id $1 via /opt/liquid_feedback_core/sendinvitation.sh" >> /var/log/lqfb/sendinvitation.log
   cd /opt/liquid_feedback_frontend/
-  echo "Member:by_id($1):send_invitation(\"/opt/liquid_feedback_frontend/config/invite_mail.txt\",\"[Liquid] Einladung zum Liquid der Piratenpartei\")" | ../webmcp/bin/webmcp_shell myconfig
+  echo "Member:by_id($1):send_invitation()" | ../webmcp/bin/webmcp_shell myconfig
 fi
 
